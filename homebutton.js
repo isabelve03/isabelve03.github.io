6c694f410e52d3a09893dd7de5509e2c83c7dc79
Isabel Veliz <iveliz21@apu.edu>
2025-05-12 17:39:50 -0700

    About Me / Mobile Fixes
    - Fixed the description for the about me page
    - fixed animation for about me text
    - hamburger navigation for mobile devices/viewport

--- homebutton.js
+++ homebutton.js
@@ -1,3 +1,24 @@
+// Show home button if not on index
 if (window.location.pathname !== "/" && !window.location.pathname.endsWith("index.html")) {
-    document.getElementById("home-button").style.display = "block";
+  document.getElementById("home-button").style.display = "block";
+}
+
+// Toggle nav menu
+function toggleMenu() {
+  const navMenu = document.getElementById("navMenu");
+  navMenu.classList.toggle("show");
+}
+
+// Close nav if clicked outside
+document.addEventListener("click", function(event) {
+  const navMenu = document.getElementById("navMenu");
+  const hamburger = document.querySelector(".hamburger");
+
+  if (
+    navMenu.classList.contains("show") &&
+    !navMenu.contains(event.target) &&
+    !hamburger.contains(event.target)
+  ) {
+    navMenu.classList.remove("show");
   }
+});
